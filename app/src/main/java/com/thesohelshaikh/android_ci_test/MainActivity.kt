23b1c7b6b0df5b50c78473ab1191317b2d8f6853
Sohel Shaikh <sohelshaikh97@gmail.com>
2022-07-23 16:20:16 +0530

    Add danger

--- app/src/main/java/com/thesohelshaikh/android_ci_test/MainActivity.kt
+++ app/src/main/java/com/thesohelshaikh/android_ci_test/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.thesohelshaikh.android_ci_test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/test/java/com/thesohelshaikh/android_ci_test/ExampleUnitTest.kt
+++ app/src/test/java/com/thesohelshaikh/android_ci_test/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.thesohelshaikh.android_ci_test
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
